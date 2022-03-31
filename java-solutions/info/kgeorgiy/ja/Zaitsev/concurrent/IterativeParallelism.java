d102de2926e8e46aee0a07f8f8797fb9b98a3cf9
Onexx <ilya7538@mail.ru>
2022-03-31 15:21:13 +0300

    HW6 finished

---
+++ java-solutions/info/kgeorgiy/ja/Zaitsev/concurrent/IterativeParallelism.java
@@ -0,0 +1,114 @@
+package info.kgeorgiy.ja.Zaitsev.concurrent;
+
+import info.kgeorgiy.java.advanced.concurrent.ScalarIP;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+
+/**
+ * Implementation of {@link ScalarIP} interface. Provides methods for processing lists in multiple threads.
+ *
+ * @author Zaitsev Ilya
+ */
+public class IterativeParallelism implements ScalarIP {
+    /**
+     * Returns maximum value.
+     *
+     * @param threads    number or concurrent threads.
+     * @param values     values to get maximum of.
+     * @param comparator value comparator.
+     * @param <T>        value type.
+     * @return maximum of given values
+     * @throws InterruptedException             if executing thread was interrupted.
+     * @throws java.util.NoSuchElementException if no values are given.
+     */
+    @Override
+    public <T> T maximum(int threads, List<? extends T> values, Comparator<? super T> comparator) throws InterruptedException {
+        return process(threads, values, ts -> ts.stream().max(comparator).orElseThrow(NoSuchElementException::new))
+                .stream().max(comparator).orElseThrow(NoSuchElementException::new);
+    }
+
+    /**
+     * Returns minimum value.
+     *
+     * @param threads    number or concurrent threads.
+     * @param values     values to get minimum of.
+     * @param comparator value comparator.
+     * @param <T>        value type.
+     * @return minimum of given values
+     * @throws InterruptedException             if executing thread was interrupted.
+     * @throws java.util.NoSuchElementException if no values are given.
+     */
+    @Override
+    public <T> T minimum(int threads, List<? extends T> values, Comparator<? super T> comparator) throws InterruptedException {
+        return maximum(threads, values, comparator.reversed());
+    }
+
+    /**
+     * Returns whether all values satisfies predicate.
+     *
+     * @param threads   number or concurrent threads.
+     * @param values    values to test.
+     * @param predicate test predicate.
+     * @param <T>       value type.
+     * @return whether all values satisfies predicate or {@code true}, if no values are given
+     * @throws InterruptedException if executing thread was interrupted.
+     */
+    @Override
+    public <T> boolean all(int threads, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException {
+        return process(threads, values, ts -> ts.stream().allMatch(predicate)).stream().allMatch(t -> t);
+    }
+
+    /**
+     * Returns whether any of values satisfies predicate.
+     *
+     * @param threads   number or concurrent threads.
+     * @param values    values to test.
+     * @param predicate test predicate.
+     * @param <T>       value type.
+     * @return whether any value satisfies predicate or {@code false}, if no values are given
+     * @throws InterruptedException if executing thread was interrupted.
+     */
+    @Override
+    public <T> boolean any(int threads, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException {
+        return !all(threads, values, predicate.negate());
+    }
+
+    private <T, R> List<R> process(int threads, List<? extends T> values, Function<List<? extends T>, R> function) throws InterruptedException {
+        threads = Math.min(threads, values.size());
+        List<List<? extends T>> lists = split(threads, values);
+        Thread[] threadsArr = new Thread[threads];
+        List<R> results = new ArrayList<>();
+        for (int i = 0; i < threads; i++) {
+            results.add(null);
+        }
+        for (int i = 0; i < threads; i++) {
+            final int ti = i;
+            threadsArr[i] = new Thread(() -> results.set(ti, function.apply(lists.get(ti))));
+            threadsArr[i].start();
+        }
+        for (int i = 0; i < threads; i++) {
+            threadsArr[i].join();
+        }
+        return results;
+    }
+
+    private <T> List<List<? extends T>> split(int threads, List<? extends T> values) {
+        List<List<? extends T>> result = new ArrayList<>();
+        int blockSize = values.size() / threads;
+        int extra = values.size() % threads;
+        int ptr = 0;
+        for (int i = 0; i < threads; i++) {
+            int curSize = blockSize + (extra > 0 ? 1 : 0);
+            result.add(values.subList(ptr, ptr + curSize));
+            extra--;
+            ptr += curSize;
+        }
+        return result;
+    }
+}
